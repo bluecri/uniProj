2f2eb045770f114e42d7d61a31e16788bc71ae24
bluecri <kjy_1234567@naver.com>
2018-03-04 22:09:59 +0900

    block manage error

---
+++ indiv_proj/Assets/_Script/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    public static InputManager instance = null;
+
+    public const int CAM_MODE_UPVIEW = 0;
+    public const int CAM_MODE_CHARVIEW = 1;
+
+    public static int s_cameraMode = CAM_MODE_UPVIEW;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
